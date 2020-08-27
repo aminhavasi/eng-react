c5c5b83c91119823adbf3e02bf35b3fdc5088392
amin havasi <pau.ahq@gmail.com>
2020-08-28 00:53:04 +0430

    new words changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Eng from './eng';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import New from './components/newWords/new';
 
 function App() {
     return (
         <div className="App">
             <Switch>
                 <Route exact path="/" component={Eng} />
-                <Route path="/new" component={null} />
+                <Route path="/new" component={New} />
                 <Route path="/learn" component={null} />
                 <Route path="/notFound" component={null} />
                 <Redirect to="/notFound" />
---
+++ src/components/newWords/new.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './new.css';
+const New = () => {
+    return (
+        <div className="new">
+            <div
+                className=""
+                style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                }}
+            >
+                <form className="mt-5 text-white">
+                    <label className="mr-1">please enater a new word:</label>
+                    <input className="form-group" placeholder="new word?" />
+                    <button
+                        className="btn btn-sm btn-success ml-1"
+                        type="submit"
+                    >
+                        Record
+                    </button>
+                </form>
+            </div>
+            <div className="text-white bg-primary text-center">
+                <a style={{ color: 'white' }} href="/learn">
+                    learned words list
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default New;
