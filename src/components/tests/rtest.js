60bf023514ea2c0ca78690061aa81c4ad656bee8
amin havasi <pau.ahq@gmail.com>
2020-08-29 23:37:50 +0430

    s

--- src/components/tests/rtest.js
+++ src/components/tests/rtest.js
@@ -3,8 +3,8 @@ import './test.css';
 const Rtest = () => {
     return (
         <div className="rtest container bg-lisght shadow">
-            you have 100 secound and 10 question that must answer . if you ready
-            please click on start
+            you have 10 question that must answer . if you ready please click on
+            start
             <div>
                 <button
                     className="btn btn-success btn-lg shadow"
@@ -13,17 +13,19 @@ const Rtest = () => {
                     start
                 </button>
             </div>
-            <div className="bg-light shadow p-5 w-50">
-                <div className=" text-right">10</div>
-                <div className="text-center">Sky</div>
-                <div className="text-center mt-3 ">
-                    <button className="btn btn-success  mx-1 my-1">
+            <div className="bg-light shadow  p-5 w-50">
+                <div className="text-center ">Sky</div>
+                <form className="text-center mt-3 ">
+                    <button
+                        className="btn btn-success  mx-1 my-1"
+                        type="submit"
+                    >
                         remamber
                     </button>
-                    <button className="btn btn-danger  mx-1 my-1">
+                    <button className="btn btn-danger  mx-1 my-1" type="submit">
                         forget
                     </button>
-                </div>
+                </form>
             </div>
         </div>
     );
