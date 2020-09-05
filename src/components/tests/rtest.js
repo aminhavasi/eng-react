b4883bbb080a2e53d072e7bb2a53668c29b36ec3
amin havasi <pau.ahq@gmail.com>
2020-09-06 02:48:38 +0430

    remember added

--- src/components/tests/rtest.js
+++ src/components/tests/rtest.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './test.css';
 import test from './../../services/fakeRTest';
 
 const Rtest = () => {
     let [testStart, setStart] = useState(false);
     let [unique, setUnique] = useState([]);
+    let [question, setQuestion] = useState([]);
+    let [selected, setSelected] = useState(0);
 
     //-random generate----------------------------
 
@@ -34,10 +36,22 @@ const Rtest = () => {
         const questions = await test();
         if (testStart === false) {
             const uniqueNumbers = await randomGenerator();
-            setUnique(uniqueNumbers);
+            await setUnique(uniqueNumbers);
+            await setSelected(uniqueNumbers[0] - 1);
+            await setQuestion(questions);
         }
 
         setStart(!testStart);
+    };
+    //---------------------------------------
+    const remember = async () => {
+        console.log(unique);
+        console.log(question);
+        if (selected === 4) {
+            setSelected(0);
+        } else {
+            setSelected((selected += 1));
+        }
     };
     return (
         <div className="rtest container bg-lisght shadow">
@@ -55,30 +69,39 @@ const Rtest = () => {
                 </button>
             </div>
             {testStart === true ? (
-                <div className="bg-light shadow  p-5 w-50">
-                    <div className="text-center "></div>
-                    <form
-                        className="text-center mt-3 "
-                        onSubmit={(e) => e.preventDefault()}
-                    >
-                        <button
-                            className="btn btn-success  mx-1 my-1"
-                            type="submit"
+                question[unique[selected] - 1] ? (
+                    <div className="bg-light shadow  p-5 w-50">
+                        <div className="text-center ">
+                            {question[unique[selected] - 1].word}
+                        </div>
+                        <form
+                            className="text-center mt-3 "
+                            onSubmit={(e) => e.preventDefault()}
                         >
-                            remamber
-                        </button>
-                        <button
-                            className="btn btn-danger  mx-1 my-1"
-                            type="submit"
-                        >
-                            forget
-                        </button>
-                    </form>
-                    <span className="badge badge-pill bg-danger text-white">
-                        10
-                    </span>
-                </div>
-            ) : null}
+                            <button
+                                className="btn btn-success  mx-1 my-1"
+                                type="submit"
+                                onClick={() => remember()}
+                            >
+                                remamber
+                            </button>
+                            <button
+                                className="btn btn-danger  mx-1 my-1"
+                                type="submit"
+                            >
+                                forget
+                            </button>
+                        </form>
+                        <span className="badge badge-pill bg-danger text-white">
+                            {question[unique[selected] - 1].score}
+                        </span>
+                    </div>
+                ) : (
+                    console.log('*****')
+                )
+            ) : (
+                console.log('amin')
+            )}
         </div>
     );
 };
